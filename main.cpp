320726bbbce7ecc15185120766513a5d622a08b0
msamatar0 <mohamedsamatar100@gmail.com>
2017-05-01 17:57:23 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<bool> hoffman(string str);
+
+int main(){
+	string s;
+	cin >> s;
+	vector<bool> bin = hoffman(s);
+}
+
+vector<bool> hoffman(string str){
+	vector<bool> bin;
+}
