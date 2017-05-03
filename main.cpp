8c78de89e0168b5fe050a841e3e416a860e414eb
msamatar0 <mohamedsamatar100@gmail.com>
2017-05-03 14:23:09 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
 
-vector<bool> hoffman(string str);
+string hoffman(string str);
 
 int main(){
 	string s;
 	cin >> s;
-	vector<bool> bin = hoffman(s);
+	string bin = hoffman(s);
 }
 
-vector<bool> hoffman(string str){
-	vector<bool> bin;
+string hoffman(string str){
+	string bin;
 }
